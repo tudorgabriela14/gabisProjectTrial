349707aa06c275926c293fa6d31c1c8eb5007dec
gabriela-geronimo <gabriela@gabis-mac-mini.local>
2018-05-09 15:40:35 +0300

    Initial Commit

---
+++ Podstel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Podstel
+//
+//  Created by Gabriela on 09/05/2018.
+//  Copyright © 2018 Gabriela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
