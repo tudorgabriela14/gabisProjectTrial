11cff33e1cca4d14e3247bb508916650fb1da18c
gabriela-geronimo <gabriela@Gabis-Mac-mini.local>
2018-05-17 18:34:54 +0300

    myFirstCommit

---
+++ Podstel/Login/Cells/SignUpCell.swift
@@ -0,0 +1,24 @@
+//
+//  SignUpCell.swift
+//  Podstel
+//
+//  Created by Gabriela on 17/05/2018.
+//  Copyright © 2018 Gabriela. All rights reserved.
+//
+
+import UIKit
+
+class SignUpCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
